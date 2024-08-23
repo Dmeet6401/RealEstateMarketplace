d20587fcc164489c363afccc28ae90dba9d7f808
Dmeet6401 <desai.meet06@gmail.com>
2024-08-23 15:32:21 +0530

    Created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import signin from './pages/signin'
+import SignUp from './pages/SignUp'
+import about from './pages/about'
+import Profile from './pages/Profile'
+
 
 export default function App() {
   return (
-    <>
-      <h1 className='text-red-100'>Real Estate Marketplace</h1>
-      <div>
-        <p className='text-red-500'>Welcome to our online real estate marketplace!</p>
-      </div>
-    </>
-  );
+    // <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/signin" element={<signin />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/about" element={<about />} />
+        <Route exact path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
