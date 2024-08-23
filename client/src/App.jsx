1abfb32b5a53c8973e0758fe22084928acf4c2ec
Dmeet6401 <desai.meet06@gmail.com>
2024-08-23 14:53:01 +0530

    installation and setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <>
+      <h1 className='text-red-100'>Real Estate Marketplace</h1>
+      <div>
+        <p className='text-red-500'>Welcome to our online real estate marketplace!</p>
+      </div>
+    </>
+  );
+}
